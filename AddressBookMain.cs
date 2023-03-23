9e210aa47753d102eb4ebe3feaa1073370b67ccb
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-03-22 15:30:20 -1200

    Welcome page

---
+++ AddressBookMain.cs
@@ -0,0 +1,18 @@
+﻿using AddressBookAssignment;
+using System;
+using System.ComponentModel;
+using System.Data;
+using System.Security.Cryptography.X509Certificates;
+
+namespace AdressBookAssignment
+{
+    class AdressBookMainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("!!!!!!Welcome to Adress Book Program!!!!!!");
+
+        }
+    }
+}
